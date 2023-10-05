82104a039483d6b68ac6968691496cfc327094aa
Антон Стефанков <144524523+antonStefankov-q@users.noreply.github.com>
2023-10-05 21:15:01 +0300

    Add files via upload

---
+++ cypress/e2e/kalifornia.cy.js
@@ -0,0 +1,85 @@
+
+describe('Проверка формы логина', function () {
+   it('Ввожу верный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Авторизация прошла успешно').should('be.visible');
+      })
+
+
+
+
+   it('Ввожу верный логин и не верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#pass').type('wrongpassword');
+        cy.get('#loginButton').should('be.enabled');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+    })
+
+   it('Проверка я забыл пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#forgotEmailButton').click();
+        cy.contains('Восстановите пароль').should('be.visible');
+        cy.get('#exitRestoreButton > .exitIcon').should('be.visible');
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.contains('Успешно отправили пароль на e-mail').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').click();
+        cy.contains('Форма логина').should('be.visible');
+
+
+
+    })
+
+   it('Проверка валидации', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').should('be.enabled');
+        cy.get('#loginButton').click();
+        cy.contains('Нужно исправить проблему валидации').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+
+
+    })
+
+      it('Правильный логин и пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#mail').type(' GerMan@Dolnikov.ru');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').should('be.enabled');
+        cy.get('#loginButton').click();
+
+
+    })
+      
+
+
+ 
+})
+
+
+
+
+
+
+
+
+
+
+
---
+++ login.js
@@ -0,0 +1,19 @@
+
+describe('Проверка формы логина', function () {
+       it('Правильный логин и пароль', function () {
+        cy.visit('https://huntingpony.com/collection/lezhanki');
+
+
+ 
+})
+
+
+
+
+
+
+
+
+
+
+
